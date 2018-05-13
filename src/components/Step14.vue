706ec6b32ed20c17dc6e8a6aa1839ad9eae11f5a
vol4tim <vol4tim@gmail.com>
2018-05-13 17:39:54 +0400

    remove step approve

--- src/components/Step14.vue
+++ src/components/Step14.vue
@@ -26,15 +26,6 @@
             <div>
               <p class="t-break" v-for="(item, i) in lis" :key="i">
                 <a :href="`https://kovan.etherscan.io/address/${item.address}`" target="_blank">{{ item.address }}</a><br/>
-                <span v-if="item.result != ''">
-                  <b>Results from IPFS: </b><a :href="`https://ipfs.io/ipfs/${item.result}`" target="_blank">{{ item.result }}</a><br/>
-                  <span v-for="(res, resIndex) in item.resultMessage" :key="resIndex">
-                    {{ res }}<br />
-                  </span>
-                </span>
-                <span v-if="item.result == ''">
-                  <b>Results from IPFS: </b>...<br/>
-                </span>
                 <b>lighthouse: </b><a :href="`https://kovan.etherscan.io/address/${item.lighthouse}`" target="_blank">{{ item.lighthouse }}</a><br/>
                 <b>from: </b><a :href="`https://kovan.etherscan.io/address/${item.from}`" target="_blank">{{ item.from }}</a><br/>
                 <b>model: </b><a :href="`https://ipfs.io/ipfs/${item.model}`" target="_blank">{{ item.model }}</a><br/>
@@ -42,6 +33,31 @@
                 <b>token: </b>{{ item.token }}<br/>
                 <b>promisee: </b>{{ item.promisee }}<br/>
                 <b>promisor: </b>{{ item.promisor }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="secw-narrow">
+        <h2>5. Results from IPFS</h2>
+    </div>
+    <div class="secw-base step-2-market">
+      <section class="sec-white">
+        <div class="row">
+          <div class="col-md-6">
+            <div>
+              <p class="t-break" v-for="(item, i) in lis" :key="i">
+                <a :href="`https://kovan.etherscan.io/address/${item.address}`" target="_blank">{{ item.address }}</a><br/>
+                <span v-if="item.result != ''">
+                  <b>Result: </b><a :href="`https://ipfs.io/ipfs/${item.result}`" target="_blank">{{ item.result }}</a><br/>
+                  <span v-for="(res, resIndex) in item.resultMessage" :key="resIndex">
+                    {{ res }}<br />
+                  </span>
+                </span>
+                <span v-if="item.result == ''">
+                  <b>Results: </b>...<br/>
+                </span>
               </p>
             </div>
           </div>
